63f8ebce2aaccc88a5bacd2d68070259395713fc
catherinerlopezv <catherinerlopezv@gmail.com>
2020-09-09 23:08:51 -0600

    Add files via upload

---
+++ Lab 1/Lab 1/Entry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_1
+{
+    public class Entry
+    {
+       
+            public string key;
+            public int value;
+
+            public Entry(string k, int v)
+            {
+                key = k;
+                value = v;
+            }
+
+            public string toString()
+            {
+                return "(" + key + "," + value + ")";
+            }
+        
+    }
+}
